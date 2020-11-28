c7249c29ae8a211167742f438c1992f0d59edec6
Andrey Mazépas <andreymazepas@gmail.com>
2020-11-28 20:37:22 -0300

    Melhoria searchbar

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -5,30 +5,27 @@ import "./SearchBar.scss";
 const SearchBar = ({ placeholder, handleSearch }) => {
   const [inputText, setInputText] = useState("");
 
-  // Captura a keypress e checa se é o ENTER
-  const keyPress = (e) => {
-    if (e.keyCode === 13) {
+  const handleSubmit = (e) => {
+      e.preventDefault();
       handleSearch(inputText);
-    }
-  };
+  }
 
   return (
-    <div className="searchbar-wrap">
+    <form onSubmit={handleSubmit} className="searchbar-wrap">
       <input
         type="text"
         className="searchbar-input"
         placeholder={placeholder}
         value={inputText}
         onChange={(e) => setInputText(e.target.value)}
-        onKeyDown={keyPress}
       />
       <button
         className="searchbar-button"
-        onClick={() => handleSearch(inputText)}
+        type="submit"
       >
         <FaSearch />
       </button>
-    </div>
+    </form>
   );
 };
 export default SearchBar;
